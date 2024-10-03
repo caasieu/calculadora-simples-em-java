8dd09403d26561ab4e336421f57b3211585a98a0
caasieu <caasim353@gmail.com>
2024-10-03 14:19:19 -0300

    adding the source code

---
+++ src/main/java/com/mycompany/Inicial/Inicial.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.Inicial;
+
+/**
+ *
+ * @author 824216008
+ */
+public class Inicial {
+    public static void main(String[] args) {
+        
+    }
+}
